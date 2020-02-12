234a7347b359a41e5be7d17f1dbf81b613a9d1ff
Anand Sharma <anasharm@cisco.com>
2020-02-12 11:44:24 -0500

    Fixing docker stuff

--- vm/bionic-lite.sh
+++ vm/bionic-lite.sh
@@ -75,11 +75,7 @@ mv diff-so-fancy /usr/local/bin
 diff-so-fancy -v
 
 # Container Tools
-apt-get update && apt-get install -y \
-  containerd.io=1.2.10-3 \
-  docker-ce=5:19.03.4~3-0~ubuntu-$(lsb_release -cs) \
-  docker-ce-cli=5:19.03.4~3-0~ubuntu-$(lsb_release -cs)
-apt-get install -y kubectl
+apt-get install -y containerd.io=1.2.10-3 docker-ce=5:19.03.4~3-0~ubuntu-$(lsb_release -cs) docker-ce-cli=5:19.03.4~3-0~ubuntu-$(lsb_release -cs) kubectl
 
 # Setup Docker daemon
 cat > /etc/docker/daemon.json <<EOF
@@ -103,4 +99,4 @@ systemctl restart docker
 echo "N" > /tmp/silent-configure
 apt-get -y update
 apt-get -y upgrade < /tmp/silent-configure
-apt -y install unattended-upgrades apt-listchanges bsd-mailx
+apt-get -y install unattended-upgrades apt-listchanges bsd-mailx
