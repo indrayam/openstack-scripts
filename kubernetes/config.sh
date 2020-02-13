12734034224c8f9412ec63adf4f830494f66ec9a
Anand Sharma <anasharm@cisco.com>
2020-02-13 14:21:05 -0500

    Cannot stand doing Python2 in 2020. Going to 3!

--- kubernetes/config.sh
+++ kubernetes/config.sh
@@ -3,8 +3,8 @@
 # Generate TOKEN
 function generate_token() {
     # TOKEN=`python2 -c 'import random; print "%0x.%0x" % (random.SystemRandom().getrandbits(3*8), random.SystemRandom().getrandbits(8*8))'`
-    TOKEN1=`python2 -c "import random; print ''.join(random.choice('0123456789abcdefghijklmnopqrstuvwxyz') for i in range(6))"`
-    TOKEN2=`python2 -c "import random; print ''.join(random.choice('0123456789abcdefghijklmnopqrstuvwxyz') for i in range(16))"`
+    TOKEN1=`python -c "import random; print ''.join(random.choice('0123456789abcdefghijklmnopqrstuvwxyz') for i in range(6))"`
+    TOKEN2=`python -c "import random; print ''.join(random.choice('0123456789abcdefghijklmnopqrstuvwxyz') for i in range(16))"`
     TOKEN="${TOKEN1}.${TOKEN2}"
 }
 
@@ -16,6 +16,6 @@ az_suffixes
 
 # How many Nodes?
 function num_of_nodes() { 
-    NUM_OF_NODES=`python2 -c 'for i in range(1, 4): print(i)'` ##CHANGEME depending upon how many nodes necessary
+    NUM_OF_NODES=`python -c 'for i in range(1, 4): print(i)'` ##CHANGEME depending upon how many nodes necessary
 }
 num_of_nodes
