1ba9656ae9d9dc72cc1654e6794965351220c6c9
Anand Sharma <anasharm@cisco.com>
2020-05-31 19:09:29 -0400

    Added more OS projects for K8s

---
+++ kubernetes/config/rcdn-code.sh
@@ -0,0 +1,15 @@
+# Setup OpenStack Cluster Details (RCDN Cluster)
+export AZ_NAME="cloud-rcdn-1-a"
+# export AZ_NAME="cloud-rcdn-1-b"
+# export AZ_NAME="cloud-rcdn-1-c"
+export FLAVOR_NAME="2vCPUx4GB"
+
+export NETWORK_ID="net-id=5e68d4a7-dddf-4687-b5d3-64cad7465810"
+export SSH_KEY="anand on macbook"
+export IMAGE_NAME="UBUNTU-18.04-CORE"
+export OS_AUTH_URL="https://cloud-rcdn-1.cisco.com:5000/v3"
+export OS_IDENTITY_API_VERSION=3
+export OS_PROJECT_NAME="CD-Services"
+export OS_PROJECT_DOMAIN_NAME="cisco"
+export OS_USERNAME="anasharm"
+export OS_USER_DOMAIN_NAME="cisco"
